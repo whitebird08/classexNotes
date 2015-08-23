9a6ae35ffbe702539d4edc32e4393be314d8f5d8
Ryan Bird <ryan01bird@gmail.com>
2015-08-23 16:53:27 -0600

    url.parse notes

--- week5/sun.js
+++ week5/sun.js
@@ -1,4 +1,15 @@
+var http = require('http');
+ var handleRequest = function (req, res) {
+   if (req.url == '/favicon.ico') return res.end();
  
+  res.end('Strings are so cool! But this string will be replaced as you experiment!');
+  // res.end(req.url);
+ };
+ 
+ var server = http.createServer(handleRequest);
+ server.listen(8000);
+ 
+-----
  var handleRequest = function (req, res) {
    if (req.url == '/favicon.ico') return res.end();
 -    res.end(req.url.slice(1));
@@ -16,3 +27,43 @@
 +    res.end(paramsStr);
      
  };
+
+ var http = require('http');
+var fs = require('fs');
+var url = require('url')
+
+var handleRequest = function (req, res) {
+  if (req.url == '/favicon.ico') return res.end();
+
+  var test = {}
+  var urlEnc1 = /[%2C]/
+  var urlEnc2 = /[%2E]/
+  var input = req.url.slice(6).replace(/[!@#$%^&*<>0-9]/g, ' ').replace(urlEnc1, '').replace(urlEnc2, '').split('=')
+
+  test[input[0]] = input[1];
+
+  res.end(JSON.stringify(test));   
+};
+
+var server = http.createServer(handleRequest);
+server.listen(8000);
+
+});
+-----------------
+https://docs.nodejitsu.com/articles/HTTP/clients/how-to-access-query-string-parameters
+
+var http = require('http');
+// var fs = require('fs');
+var url = require('url')
+
+var handleRequest = function (req, res) {
+  if (req.url == '/favicon.ico') return res.end();
+
+
+var parsedStr = url.parse(req.url, true).query
+ res.end(JSON.stringify(parsedStr));  
+};
+
+var server = http.createServer(handleRequest);
+server.listen(8000);
+
