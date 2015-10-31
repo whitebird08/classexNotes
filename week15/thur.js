771fb9e2633dc7dae03bc8c2c0638fa5ceaf0d52
Ryan Bird <ryan01bird@gmail.com>
2015-10-30 19:44:36 -0600

    practicing scales

---
+++ week15/thur.js
@@ -0,0 +1,24 @@
+// function declartion 
+function sumDeclartion(arg1, arg2){
+  console.log(arg1 + arg2);
+}
+sumDeclartion(1,2);
+
+
+// function expression
+var sumExpression = function(arg1, arg2) {
+  console.log(arg1 + arg2);
+}
+sumExpression(1,2);
+
+
+// IIFE 
+(function (arg1,arg2){
+  console.log(arg1 + arg2);
+})(1, 2); 
+
+
+
+
+
+
