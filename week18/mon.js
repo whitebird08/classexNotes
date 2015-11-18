785c7c9dbb9eccaff132a780d3617f07a28545ad
Ryan Bird <ryan01bird@gmail.com>
2015-11-17 19:29:09 -0700

    added piechart files

---
+++ week18/mon.js
@@ -0,0 +1,9 @@
+var arr = [1,3,5,2,6,8,4]
+// function sorted(arr){
+//   var newArr = [];
+//   arr.filter(function(){
+//     arr.sort()
+//   })
+//   return newArr
+// }
+console.log(arr.sort())
